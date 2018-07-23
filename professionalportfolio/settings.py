e7ff5c358319b9df029e9202ceb477d191535714
Sindhu Vempati <sv0014@mix.wvu.edu>
2018-07-23 15:03:53 -0400

    Jobs App Up

--- professionalportfolio/settings.py
+++ professionalportfolio/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'professionalportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql', #put postgres
+        'NAME': 'portfoliodb',
+        'USER' : 'postgres',
+        'PASSWORD':'0326',
+        'HOST':'localhost',
+        'PORT':'5432',
+
     }
 }
 
