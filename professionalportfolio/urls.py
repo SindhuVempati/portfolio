6354531ef1fb3691ef2cb7d6815be100c98de4fb
Sindhu Vempati <sv0014@mix.wvu.edu>
2018-07-24 20:56:17 -0400

    UI completed

--- professionalportfolio/urls.py
+++ professionalportfolio/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 from django.conf import settings #Add this to get the settings.py
 from django.conf.urls.static import static #for static files
+import jobs.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',jobs.views.home, name='home'),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) #for dynamic image url click
